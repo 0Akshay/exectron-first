a95adac756a162f2e38748b0247d85436ea47137
Akshay <akshaythacmp@gmail.com>
2026-03-14 20:45:20 +0530

    Make it working

--- preload.js
+++ preload.js
@@ -1,10 +1,5 @@
 const { contextBridge } = require("electron");
-const path = require("path");
-// const db = require(path.join(__dirname, "database", "db"));
-
-// console.log("preload directory:", __dirname);
 
 contextBridge.exposeInMainWorld("api", {
-    hello: () => "Hello from Electron",
-    // addUser: (name) => db.addUser(name)
+    hello: () => "Hello from Electron"
 });
